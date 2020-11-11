227afdac52b8d22eefcc93b0a37be701e84f9052
Taru Vikström <62556762+everleee@users.noreply.github.com>
2020-11-11 10:49:10 +0200

    Main Menu controller for 2D game

---
+++ MainMenuController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public GameObject saveSound;
+   
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Story");
+    }
+
+    public void Save()
+    {
+        GameStatus.status.Save();
+        Destroy(Instantiate(saveSound), saveSound.GetComponent<AudioSource>().clip.length);
+    }
+
+    public void Load()
+    {
+        GameStatus.status.Load();
+        if (GameStatus.status.Story == true)
+        {
+            SceneManager.LoadScene("Level1");
+        } else
+        {
+            SceneManager.LoadScene("Story");
+        }
+
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+
+    }
+
+
+}
